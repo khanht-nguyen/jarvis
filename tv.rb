93926e9bc8af15f847ef5621bad3798dfd384128
root <khanhtnguyen@live.com>
2015-07-28 15:32:34 +0000

    initial

---
+++ tv.rb
@@ -0,0 +1,24 @@
+require './server'
+require 'yaml'
+
+config = YAML.load_file(__dir__ + "/config.yml")
+
+if config == false then
+    raise('Either the configuration file could not be found or nothing was in there!')
+end
+
+threads = []
+
+config.each do |k, v|
+    threads << Thread.new do 
+        puts 'Setting up channel: ' + k
+        server = Server.new(v['port'], v['debug_level'], v['key'])
+        server.serve()
+    end
+end
+
+begin
+    threads.each{ |t| t.join }
+rescue Exception
+    puts 'Good bye!'
+end
